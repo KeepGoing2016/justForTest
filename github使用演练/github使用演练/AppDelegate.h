05ef59ad7988716bfc8061c7e43c261baabe6ba0
lumf <lumf@lumfdemac-mini.local>
2016-03-22 10:47:13 +0800

    提交测试工程

---
+++ github使用演练/github使用演练/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github使用演练
+//
+//  Created by lumf on 16/3/22.
+//  Copyright © 2016年 lumf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github使用演练/github使用演练/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github使用演练
+//
+//  Created by lumf on 16/3/22.
+//  Copyright © 2016年 lumf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
